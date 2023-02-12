8c64df14f9d7d447725933822235d5b28810c412
MOkhten <m.okhten@gmail.com>
2023-02-12 21:09:04 +0200

    styles

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,11 +1,11 @@
 import { Outlet } from 'react-router-dom';
-import AppBar from './AppBar/AppBar';
+import ResponsiveAppBar from './AppBar/AppBar';
 import { Suspense } from 'react';
 
 const Layout = () => {
   return (
     <div style={{ maxWidth: 960, margin: '0 auto', padding: '0 16px' }}>
-      <AppBar />
+      <ResponsiveAppBar />
       <Suspense fallback={null}>
         <Outlet />
       </Suspense>
--- src/components/NavLink/NavLink.styled.js
+++ src/components/NavLink/NavLink.styled.js
@@ -5,11 +5,17 @@ export const StyledLink = styled(NavLink)`
   padding: 8px 16px;
   border-radius: 4px;
   text-decoration: none;
-  color: #000000;
-  font-weight: 500;
-  display: inline-block;
-  &.active {
-    color: #FFFFFF;
-    background-color: #1976d2;
-  }
+  font-size: 1.25rem;
+    line-height: 1.6;
+    letter-spacing: 0.0075em;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    white-space: nowrap;
+    color:  #FFFFFF;
+    margin-right: 16px;
+    font-family: monospace;
+    font-weight: 700;
+    transition: box-shadow 300ms cubic-bezier(0.4, 0, 0.2, 1) 0ms;
+    box-shadow: 0px 2px 4px -1px rgb(0 0 0 / 20%), 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%);
+    
 `;
